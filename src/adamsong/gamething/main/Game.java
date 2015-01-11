ec65eeeeda3462c383b3af517e3a388f5ec0114a
Gabriel Adamson <adamson.g@gmail.com>
2015-01-11 08:52:42 -0500

    Create initial files

---
+++ src/adamsong/gamething/main/Game.java
@@ -0,0 +1,16 @@
+package adamsong.gamething.main;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public Game(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
